0b76c942273183b918a1d711feb8c74e3194eed0
Babaroki <karvarrohat@gmail.com>
2026-03-22 23:03:59 +0300

    Update node.js

--- node.js
+++ node.js
@@ -3,5 +3,5 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Bot aktif"));
 app.listen(3000, () => console.log("Server çalışıyor"));
-host: 'SUNUCU_IP',
+host: 'babaprosmp18281.aternos.me',
   username: 'RastgeleBirIsim123'
